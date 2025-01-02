ebb805bfa70e0f08259c89769ee6f5d66e35461c
CanerYesiltas <caneryesiltas1@gmail.com>
2025-01-02 13:59:42 -0500

    updated new routes more modern practice

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import './styles/App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import Home from './pages/home/Home'
-import Create from './pages/create/Create'
-import Details from './pages/details/Details'
-import Search from './pages/search/Search'
+import Home from './pages/home/Home';
+import Create from './pages/create/Create';
+import Details from './pages/details/Details';
+import Search from './pages/search/Search';
+
+const routes = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: 'create', element: <Create /> },
+  { path: 'details/:id', element: <Details /> },
+  { path: 'search', element: <Search /> },
+]);
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/create' element={<Create />} />
-        <Route path='/details/:id' element={<Details />} />
-        <Route path='/search' element={<Search />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={routes}/>;
 }
 
 export default App;
