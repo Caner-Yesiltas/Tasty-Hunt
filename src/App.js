b740d79a81c4a4ec82a0ad24281c19af408fc39b
CanerYesiltas <caneryesiltas1@gmail.com>
2025-01-01 21:20:50 -0500

    add router and layouts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import './styles/App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/home/Home'
+import Create from './pages/create/Create'
+import Details from './pages/details/Details'
+import Search from './pages/search/Search'
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/create' element={<Create />} />
+        <Route path='/details/:id' element={<Details />} />
+        <Route path='/search' element={<Search />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
