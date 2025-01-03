a2849ed5a16ac1fdd6b4cec49f9f8a444e37adf7
CanerYesiltas <caneryesiltas1@gmail.com>
2025-01-03 16:34:02 -0500

    add post request

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,45 +1,61 @@
 const { useState, useEffect } = require('react');
 
-const useFetch = (url) => {
+const useFetch = (url, method="GET") => {
   const [data, setData] = useState(null);
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState(null)
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [options, setOptions] = useState(null);
+
+  const postData = (data) =>{
+    setOptions({
+      method:"POST",
+      headers:{
+        "Content-Type":"aplication/json"
+      },
+      body:JSON.stringify(data)
+    })
+  }
 
   useEffect(() => {
     // fetch(url)
     //  .then((res) => res.json())
     //  .then((data) => setData(data));
 
-    const fetchData = async () => {
-        setIsLoading(true);
+    const fetchData = async (options) => {
+      setIsLoading(true);
 
-        try {
-            const res = await fetch(url);
-            if (!res.ok) {
-                throw new Error(res.statusText);
-            }
-
-            const data = await res.json();
-            setIsLoading(false);
-            setData(data);
-
-        } catch (error) {
-            setIsLoading(false);
-            setError("error");
-            console.log(error.message);
-            
+      try {
+        const res = await fetch(url, {...options});
+        if (!res.ok) {
+          throw new Error(res.statusText);
         }
 
-
-
-     
+        const data = await res.json();
+        setIsLoading(false);
+        setData(data);
+      } catch (error) {
+        setIsLoading(false);
+        setError('error');
+        console.log(error.message);
+      }
     };
 
+      if(method==="GET") {
+        fetchData();
+      }
+
+      if(method==="POST" && options) {
+        fetchData(options)
+      }
+
     fetchData();
-  }, [url]);
+  }, [url, options, method]);
 
   return {
-    data, isLoading, error
+    data,
+    isLoading,
+    error,
+    postData,
   };
 };
 
--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,82 +1,129 @@
 import React, { useRef, useState } from 'react';
 import './create.css';
+import useFetch from '../../hooks/useFetch';
 
 const Create = () => {
-  const [title, setTitle] = useState('')
-  const [description, setDescription] = useState('')
-  const [preparation, setPreparation] = useState('')
-  const [image, setImage] = useState('')
-  const [ingredient, setIngredient] = useState('')
-  const [ingredients, setIngredients] = useState([])
-  const [url, setUrl] = useState('')
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [preparation, setPreparation] = useState('');
+  const [image, setImage] = useState('');
+  const [ingredient, setIngredient] = useState('');
+  const [ingredients, setIngredients] = useState([]);
+  const [url, setUrl] = useState('');
   const ingredientInput = useRef(null);
 
-const handleingredients = (e) => {
-const item= ingredient.trim();
+  const { postData } = useFetch("http://localhost:3000/recipes", "POST")
 
+  const handleingredients = (e) => {
+    const item = ingredient.trim();
 
-if(item && !ingredients.includes(item)) {
-  setIngredients(prevItems =>[...prevItems, item])
-}
-setIngredient('');
-ingredientInput.current.focus();
-ingredientInput.current.value='';
+    if (item && !ingredients.includes(item)) {
+      setIngredients((prevItems) => [...prevItems, item]);
+    }
+    setIngredient('');
+    ingredientInput.current.focus();
+ 
+  };
 
-}
-
-
-  const handleSubmit= (e) =>{
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(title, description,preparation,image,url);
-    
-  }
+   postData({title, description, ingredients, preparation, image, url});
+  };
 
   return (
     <div className='card mt-3'>
       <div className='card-body'>
         <h2>Add New Recipe</h2>
-        <form onSubmit={handleSubmit}  >
+        <form onSubmit={handleSubmit}>
           <div className='mb-3'>
-            <label htmlFor='title' className='from-label'>Title</label>
-            <input type="text" name="title" id="title" className='form-control' onChange={(e)=>setTitle(e.target.value)} />
+            <label htmlFor='title' className='from-label'>
+              Title
+            </label>
+            <input
+              type='text'
+              name='title'
+              id='title'
+              className='form-control'
+              onChange={(e) => setTitle(e.target.value)}
+            />
           </div>
           <div className='mb-3'>
-            <label htmlFor='description' className='from-label'>Description</label>
-            <input type="text" name="description" id="description" className='form-control' onChange={(e)=>setDescription(e.target.value)} />
+            <label htmlFor='description' className='from-label'>
+              Description
+            </label>
+            <input
+              type='text'
+              name='description'
+              id='description'
+              className='form-control'
+              onChange={(e) => setDescription(e.target.value)}
+            />
           </div>
-          <div className="mb-3"></div>
-          <label htmlFor="ingredients">Ingredients <ul>  {ingredients.map(item=> <li key={item} > {item} </li> )} </ul>  </label>
-          <div className="input-group d-flex justify-content-between align-items-center">
-            
-  <input
-    type="text"
-    ref={ingredientInput}
-    name="ingredients"
-    className="form-control"
-    placeholder="Add ingredient"
-    onChange={(e) => setIngredient(e.target.value)}
-  />
-  <button
-    className="btn btn-secondary"
-    type="button"
-    onClick={handleingredients}
-  >
-    +
-  </button>
-</div>
+          <div className='mb-3'></div>
+          <label htmlFor='ingredients'>
+            Ingredients{' '}
+            <ul>
+              {' '}
+              {ingredients.map((item) => (
+                <li key={item}> {item} </li>
+              ))}{' '}
+            </ul>{' '}
+          </label>
+          <div className='input-group d-flex justify-content-between align-items-center'>
+            <input
+              type='text'
+              ref={ingredientInput}
+              name='ingredients'
+              className='form-control'
+              placeholder='Add ingredient'
+              onChange={(e) => setIngredient(e.target.value)}
+            />
+            <button
+              className='btn btn-secondary'
+              type='button'
+              onClick={handleingredients}
+            >
+              +
+            </button>
+          </div>
           <div className='mb-3'>
-            <label htmlFor='preparation' className='from-label'>Preparation</label>
-          <textarea name="preparation" id="preparation" className='form-control' onChange={(e)=>setPreparation(e.target.value)}  ></textarea>
+            <label htmlFor='preparation' className='from-label'>
+              Preparation
+            </label>
+            <textarea
+              name='preparation'
+              id='preparation'
+              className='form-control'
+              onChange={(e) => setPreparation(e.target.value)}
+            ></textarea>
           </div>
           <div className='mb-3'>
-            <label htmlFor='image' className='from-label'>Image</label>
-            <input type="text" name="image" id="image" className='form-control'onChange={(e)=>setImage(e.target.value)}  />
+            <label htmlFor='image' className='from-label'>
+              Image
+            </label>
+            <input
+              type='text'
+              name='image'
+              id='image'
+              className='form-control'
+              onChange={(e) => setImage(e.target.value)}
+            />
           </div>
           <div className='mb-3'>
-            <label htmlFor='url' className='from-label'>Url</label>
-            <input type="text" name="url" id="url" className='form-control' onChange={(e)=>setUrl(e.target.value)} />
+            <label htmlFor='url' className='from-label'>
+              Url
+            </label>
+            <input
+              type='text'
+              name='url'
+              id='url'
+              className='form-control'
+              onChange={(e) => setUrl(e.target.value)}
+            />
           </div>
-          <button type="submit" className='btn btn-primary'>Save</button>
+          <button type='submit' className='btn btn-primary'>
+            Save
+          </button>
         </form>
       </div>
     </div>
